93e6b41e34a36476fc6373d7ab0b53487a1fb701
Michael Carreño Ramón <michaelcarreno30@gmail.com>
2025-07-02 01:10:35 +0000

    Update with langsmith support

--- moneychanger.py
+++ moneychanger.py
@@ -7,6 +7,7 @@ import json
 import streamlit as st
 import os
 from openai import OpenAI
+from langsmith import wrappers, traceable
 
 token = os.environ["GITHUB_TOKEN"]
 endpoint = "https://models.inference.ai.azure.com"
@@ -20,6 +21,11 @@ client = OpenAI(
 load_dotenv()
 EXCHANGERATE_API_KEY = os.getenv('EXCHANGERATE_API_KEY')
 
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = "cursomoneychanger"
+
+@traceable
 def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
     """Return a tuple of (base, target, amount, conversion_result (2 decimal places))"""
     url = f"https://v6.exchangerate-api.com/v6/{EXCHANGERATE_API_KEY}/pair/{base}/{target}/{amount}"
@@ -28,7 +34,7 @@ def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
 
 print(get_exchange_rate("USD", "GBP", "350"))
 
-
+@traceable
 def call_llm(textbox_input) -> Dict:
     """Make a call to the LLM with the textbox_input as the prompt.
        The output from the LLM should be a JSON (dict) with the base, amount and target"""
@@ -86,7 +92,7 @@ def call_llm(textbox_input) -> Dict:
     else:
         return response#.choices[0].message.content
 
-
+@traceable
 def run_pipeline(user_input):
     """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
     Call get_exchange_rate(...) if necessary"""
